d7ac45356536a6d0230484c298ae72cf1996b897
jverbosk <jverbosk@gmail.com>
2017-01-11 23:53:49 -0500

    added comments to exercise 3

--- ex3.rb
+++ ex3.rb
@@ -3,26 +3,40 @@
 # Yes... I'm typing this all out by hand
 # I love to type...  ^_^;
 
+# This line just outputs the string
 puts "I will now count my chickens:"
 
+# This line outputs: Hens 30
 puts "Hens #{25 + 30 / 6}"
+# This line outputs: Rosters 97
 puts "Roosters #{100 - 225 * 3 % 4}"
 
+# This line just outputs the string again
 puts "Now I will count the eggs:"
 
+# This line outputs: 7
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
+# This line yet again outputs the string verbatim
 puts "Is it true that 3 + 2 < 5 - 7?"
 
+# This line outputs (evaluates to): false
 puts 3 + 2 < 5 - 7
 
+# This line prints the string and evaluates to 5
 puts "What is 3 + 2? #{3 + 2}"
+# This line prints the string and evaluates to -2
 puts "What is 5 - 7? #{5 - 7}"
 
+# This line prints the string verbatim again
 puts "Oh, that's why it's false."
 
+# This line prints the string verbatim as well
 puts "How about some more."
 
+# This line prints the string and evaluates to: true
 puts "Is it greater? #{5 > -2}"
+# This line prints the string and also evaluates to: true
 puts "is it greater or equal? #{5 >= -2}"
-puts "Is it less or equal #{5 <= -2}"
+# This line prints the string and evaluates to: false
+puts "Is it less or equal? #{5 <= -2}"
