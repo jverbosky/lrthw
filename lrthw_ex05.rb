eb7c34fc15cc16f0c3ac1ac05034817e0b55020f
jverbosk <jverbosk@gmail.com>
2017-01-19 21:58:07 -0500

    completed exercise 5

--- lrthw_ex05.rb
+++ lrthw_ex05.rb
@@ -1,20 +1,39 @@
 # Learn Ruby the Hard Way
 # Exercise 5: More Variables and Printing
 
-my_name = 'Zed A. Shaw'
-my_age = 35 # not a lie in 2009
-my_height = 74 # inches
-my_weight = 180 # lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Zed A. Shaw'
+age = 35 # not a lie in 2009
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
 
-puts "Let's talk about #{my_name}."
-puts "He's #{my_height} inches tall."
-puts "He's #{my_weight} pounds heavy."
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall."
+puts "He's #{weight} pounds heavy."
 puts "Actually that's not too heavy."
-puts "He's got #{my_eyes} eyes and #{my_hair} hair."
-puts "His teeth are usually #{my_teeth} depending on the coffee."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
 
 # this line is tricky, try to get it exactly right
-puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
+
+# 1) Glad I'm using Sublime Text... that took 2 seconds.  ^_^
+# 2) Done:
+
+def in_to_cm(inches)
+  inches_to_centimeters = inches * 2.54
+  puts "Your measurement in centimeters is #{inches_to_centimeters} centimeters."
+end
+
+def lb_to_kg(pounds)
+  pounds_to_kilograms = pounds * 0.453592
+  puts "Your measurement in pounds is #{pounds_to_kilograms} kilograms."
+end
+
+in_to_cm(height)
+lb_to_kg(weight)
+
+
+
