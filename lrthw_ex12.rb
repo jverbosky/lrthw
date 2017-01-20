f940ccd27d8054ffeabe31eaf81a26236d87eda7
jverbosk <jverbosk@gmail.com>
2017-01-19 23:42:32 -0500

    pushing exercise 12 before study drill changes

--- lrthw_ex12.rb
+++ lrthw_ex12.rb
@@ -1,3 +1,15 @@
 # Learn Ruby the Hard Way
 # Exercise 12: Prompting People
 
+print "Give me a number: "
+number = gets.chomp.to_i
+
+bigger = number * 100
+puts "A bigger number is #{bigger}."
+
+print "Give me another number: "
+another = gets.chomp
+number = another.to_i
+
+smaller = number / 100
+puts "A smaller number is #{smaller}."
