3383b1b41562f0682bb1c9f37f74b1d3fa5b1c47
jverbosk <jverbosk@gmail.com>
2017-01-20 00:32:21 -0500

    completed exercise 13 and extra script

--- lrthw_ex13.rb
+++ lrthw_ex13.rb
@@ -1,3 +1,23 @@
 # Learn Ruby the Hard Way
 # Exercise 13: Parameters, Unpacking, Variables
 
+# Note - have to provide arguments when executing .rb script when using ARGV
+# Ex: ruby lrthw_ex13.rb What is it?
+
+# Specify the variables to be assigned to user-specified values when the .rb script runs
+first, second, third = ARGV
+
+# Prints the specified string along with the interpolated value for the "first" variable
+puts "Your first variable is: #{first}"
+
+# Prints the specified string along with the interpolated value for the "first" variable
+puts "Your second variable is: #{second}"
+
+# Prints the specified string along with the interpolated value for the "first" variable
+puts "Your third variable is: #{third}"
+
+# 0) Comments added
+# 1) When an argument isn't specified, the variable isn't assigned a value
+# 2) Any additional arguments are dropped/ignored
+# 3) Done in lrthw_ex13_script.rb
+# 4) See 3).
---
+++ lrthw_ex13_script.rb
@@ -0,0 +1,23 @@
+# Learn Ruby the Hard Way
+# Exercise 13: Parameters, Unpacking, Variables
+# Example script per Study Drills steps three and four
+
+print "There are two doors in front of you.\nWhich will you open - the door on the left or the right? "
+door = $stdin.gets.chomp 
+#door = ARGV
+print ".....\n"
+print "Now you are standing in a room with stairs going up and down.\nWhich way, pal? "
+stairs = $stdin.gets.chomp
+print ".....\n"
+print "You slip on the stairs, have a terrible spill and end up breaking your leg.\nWhat now? "
+action = $stdin.gets.chomp
+action = action.downcase!
+print ".....\n"
+print "As you #{action}, an earthquake starts shaking the walls apart. This just isn't your day!\nAny final words? "
+final_words = $stdin.gets.chomp
+final_words = final_words.downcase!
+print ".....\n"
+puts "In your last moments of conciousness, your life flashes before your eyes.
+You see yourself opening the door on the #{door} and going #{stairs} the stairs.
+You decide to #{action} after falling down the stairs and breaking your leg.
+Sadly, your final cry of \"#{final_words}\"  is silenced prematurely as the walls come down upon you."
