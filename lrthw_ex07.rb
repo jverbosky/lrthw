fd4adf897e3861d262548a0100ff6779c42c73d0
jverbosk <jverbosk@gmail.com>
2017-01-19 22:24:52 -0500

    completed exercise 7

--- lrthw_ex07.rb
+++ lrthw_ex07.rb
@@ -1,3 +1,66 @@
 # Learn Ruby the Hard Way
 # Exercise 7: More Printing
 
+# Prints the specified string
+puts "Mary had a little lamb."
+
+# Prints the specified string and interpolated string
+puts "Its fleece was white as #{'snow'}."
+
+# Prints the specified string
+puts "And everywhere that Mary went."
+
+# Prints a period ten times
+puts "." * 10 # what'd that do?
+
+# Assigns a string to the variable end1
+end1 = "C"
+
+# Assigns a string to the variable end2
+end2 = "h"
+
+# Assigns a string to the variable end3
+end3 = "e"
+
+# Assigns a string to the variable end4
+end4 = "e"
+
+# Assigns a string to the variable end5
+end5 = "s"
+
+# Assigns a string to the variable end6
+end6 = "e"
+
+# Assigns a string to the variable end7
+end7 = "B"
+
+# Assigns a string to the variable end8
+end8 = "u"
+
+# Assigns a string to the variable end9
+end9 = "r"
+
+# Assigns a string to the variable end10
+end10 = "g"
+
+# Assigns a string to the variable end11
+end11 = "e"
+
+# Assigns a string to the variable end12
+end12 = "r"
+
+# watch that print vs. puts on this line what's it do?
+
+# The print statement concatenates the strings assigned to each specified variable,
+# without putting a line return at the end
+print end1 + end2 + end3 + end4 + end5 + end6
+
+# The puts statement concatenates the strings assigned to each specified variable,
+# while putting a line return at the end
+puts end7 + end8 + end9 + end10 + end11 + end12
+
+# 1) Comments done.
+# 2) No errors this time around.
+# 3) Will do.
+# 4) Good plan.
+# 5) Oh yeah - hundreds, if not thousands, every day.  ^_^;
